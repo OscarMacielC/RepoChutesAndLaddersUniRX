4e1d92afe82b975585931ffbf802966a7be8546c
Oscar Maciel C <oscarmacielc@live.com.mx>
2024-08-03 13:44:27 -0600

    Base commits adding namespaces

--- ChutesAndLaddersUniRX/Assets/_MAIN/Scripts/Deck/DeckManager.cs
+++ ChutesAndLaddersUniRX/Assets/_MAIN/Scripts/Deck/DeckManager.cs
@@ -1,17 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DeckManager : MonoBehaviour
+namespace ChutesAndLadders.Managers
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    public partial class DeckManager
     {
         
     }
---
+++ ChutesAndLaddersUniRX/Assets/_MAIN/Scripts/Zenject/MainInstaller.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using Zenject;
+
+namespace ChutesAndLadders
+{
+    public class MainInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+        }
+    }    
+}
